1a16646b08500894ea5e1dea9eb01af603853daa
Paweł Sacawa <psacawa@math.toronto.edu>
2019-11-25 19:41:45 -0500

    Add: print_tab_dict robi to samo z słownkami

--- ps_utils.py
+++ ps_utils.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 
-def print_tab_iter(it, attr):
+def print_tab_obj(it, attr):
     from tabulate import tabulate
     data = [[getattr(t, at, None) for at in attr] for t in it]
+    print(tabulate(data, headers=attr))
+
+def print_tab_dict(it, attr):
+    from tabulate import tabulate
+    data = [[t[at] for at in attr if at in t.keys ()] for t in it]
     print(tabulate(data, headers=attr))
