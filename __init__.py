52ed4337f37413ef6d7f9ecc9f319fbb00f11d2f
Paweł Sacawa <psacawa@math.toronto.edu>
2020-03-01 16:39:05 -0500

    Add: print_sql util

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 
-from .ps_utils import *
+from .ps_utils import print_tab_obj, print_tab_dict, print_attributes, print_sql
--- ps_utils.py
+++ ps_utils.py
@@ -1,20 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 
 from typing import Optional, List
 from tabulate import tabulate
 from collections import OrderedDict
 
+
 def print_tab_obj(it, attr):
     from tabulate import tabulate
+
     data = [[getattr(t, at, None) for at in attr] for t in it]
     print(tabulate(data, headers=attr))
 
+
 def print_tab_dict(it, attr):
     from tabulate import tabulate
-    data = [[t[at] for at in attr if at in t.keys ()] for t in it]
+
+    data = [[t[at] for at in attr if at in t.keys()] for t in it]
     print(tabulate(data, headers=attr))
 
-def print_attributes(obj, attr:Optional [List[str]]= None, trim=True, use_str=True):
+
+def print_attributes(obj, attr: Optional[List[str]] = None, trim=True, use_str=True):
     """
     Print attribute names in one column, values in the other. If not list of 
     attributes is supplies, the attributes are gather from `dir()`.
@@ -23,21 +28,38 @@ def print_attributes(obj, attr:Optional [List[str]]= None, trim=True, use_str=Tr
     """
     ncolumns = 80
     if attr == None:
-        attr = dir (obj)
-    table = OrderedDict ()
-    for at in dir (obj):
+        attr = dir(obj)
+    table = OrderedDict()
+    for at in dir(obj):
         try:
-            table[at] = getattr (obj,at)
+            table[at] = getattr(obj, at)
         except Exception as e:
-            table [at] = ""
-    table = list (table.items ())
-    table = [list (kv_pair) for kv_pair in table]
+            table[at] = ""
+    table = list(table.items())
+    table = [list(kv_pair) for kv_pair in table]
 
     for row in table:
         if use_str:
-            row[1] = str (row[1])
+            row[1] = str(row[1])
         else:
-            row[1] = repr (row[1])
+            row[1] = repr(row[1])
         if trim:
-            row[1] =  row[1][:ncolumns]
-    print (tabulate (table, headers = ['name', 'value']))
+            row[1] = row[1][:ncolumns]
+    print(tabulate(table, headers=["name", "value"]))
+
+
+def print_sql(raw_sql: str):
+    """Wrapper to format raw SQL statements."""
+    import sqlparse
+    from pygments import highlight
+    from pygments.lexers import SqlLexer
+    from pygments.formatters import TerminalTrueColorFormatter
+    from pygments.styles import get_style_by_name
+
+    style = get_style_by_name("colorful")
+    formatter = TerminalTrueColorFormatter(style=style)
+    lexer = SqlLexer()
+    formatted_sql = sqlparse.format(raw_sql, reindent=True, keyword_case="upper")
+    highlighted_sql = highlight(formatted_sql, lexer, formatter)
+
+    print(highlighted_sql)
